fc4bba079e83ec51d06f2567ccf0ebc84b932fda
Prince Pal <princepal262501@gmail.com>
2026-03-17 18:27:59 +0530

    successfully data fetched by createAsyncThunk method

--- 29_todoRedux/src/components/Data.jsx
+++ 29_todoRedux/src/components/Data.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchUsers } from '../redux/slices/dataSlice';
 
 const Data = () => {
 
-  return (
-    <div>
-      
-    </div>
-  )
+    const dispatch = useDispatch();
+    const { users, loading, error } = useSelector((state) => state.users)
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [dispatch])
+
+    useEffect(() => {
+        console.log("Updated users:", users);
+    }, [users]);
+
+
+
+
+    return (
+        <div className='grid grid-cols-4 gap-10 p-10'>
+
+              {loading && <h3>Loading...</h3>}
+              {error && <h3>{error}</h3>}
+            {users.map((elem) => <p key={elem.id}>{elem.name}</p>)}
+
+        </div>
+    )
 }
 
 export default Data
--- 29_todoRedux/src/redux/reducer.js
+++ 29_todoRedux/src/redux/reducer.js
@@ -2,6 +2,6 @@ import { combineReducers} from '@reduxjs/toolkit'
 import { dataSlice } from './slices/dataSlice'
 
 export const rootReducer = combineReducers({
-    data : dataSlice.reducer
+    users : dataSlice.reducer
 
 })
